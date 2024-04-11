01ca39df50eb132f24f7e130b4f0a6c5ba20354f
wtjerry <wtjerry123@gmail.com>
2024-04-11 18:44:06 +0200

    add projects

---
+++ src/FsharpGrpcGreeterService.Tests/FsharpGrpcGreeterServiceVersionTests.cs
@@ -0,0 +1,35 @@
+﻿namespace FSharpInActionGrpc.FsharpGrpcGreeterService;
+
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using FluentAssertions;
+using Xunit;
+
+public partial class FsharpGrpcGreeterServiceVersionTests
+{
+    private readonly Assembly assembly = typeof(Program).Assembly;
+
+    [Fact]
+    public void IsVersioned()
+    {
+        var version = this.assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+        version!.InformationalVersion.Should().MatchRegex(
+            Version(),
+            "the version should be suffixed by the git hash with dirty flag");
+    }
+
+    [Fact]
+    public void IsTimestamped()
+    {
+        var metadata = this.assembly.GetCustomAttributes<AssemblyMetadataAttribute>();
+        metadata.Single(m => m.Key == "BuildDate").Value.Should().Match(buildDate => Timestamp(buildDate));
+    }
+
+    private static bool Timestamp(string value) => DateTime.TryParse(value, CultureInfo.InvariantCulture, out _);
+
+    [GeneratedRegex(@"\d.\d.\d\+[a-z0-9]{7,}\*?")]
+    private static partial Regex Version();
+}
---
+++ src/FsharpGrpcGreeterService/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace FSharpInActionGrpc.FsharpGrpcGreeterService;
+
+using System;
+using System.Threading.Tasks;
+
+public static class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        await Console.Error.WriteLineAsync($"Got {args.Length - 1} arguments");
+        return 0;
+    }
+}
